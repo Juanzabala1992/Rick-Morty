4bd5f3c692ec9ea77efef4ffb08c32f85d1dabc7
Juan Zabala <jzabala@cobiscorp.com>
2021-11-07 14:44:32 -0500

    Creando estructura del proyecto

--- proyecto-RickAndMorty/src/app/app.module.ts
+++ proyecto-RickAndMorty/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { SearchComponent } from './shared/components/search/search.component';
+import { CharacterComponent } from './components/characters/character/character.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchComponent,
+    CharacterComponent
   ],
   imports: [
     BrowserModule,
